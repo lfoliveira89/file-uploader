4a1f892d2851b90545d37cc3c6c7b403c7b9edaf
Luis Fernando <luis_fernando@gap.com>
2017-11-14 10:12:51 -0200

    fixed large files download

--- src/main/java/fileuploader/controller/FileUploadController.java
+++ src/main/java/fileuploader/controller/FileUploadController.java
@@ -22,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.time.Instant;
 import java.util.List;
@@ -81,7 +82,7 @@ public class FileUploadController {
 
         UploadedFileResource uploadedFileResource = storageService.findById(valueOf(id));
         response.setHeader("Content-Disposition", "attachment; filename=" + uploadedFileResource.getFilename());
-        IOUtils.copy(uploadedFileResource.getInputStream(), response.getOutputStream());
+        IOUtils.copy(uploadedFileResource.getInputStream(), new BufferedOutputStream(response.getOutputStream()));
     }
 
     @PostMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
@@ -89,7 +90,8 @@ public class FileUploadController {
                                          @RequestParam(value = FILE_PARAM) MultipartFile file,
                                          @RequestHeader(value = CONTENT_RANGE_HEADER, required = false) String contentRange)
             throws MissingServletRequestParameterException {
-        log.info("[FileUploadController.upload] uploading file {} for userId {}. Content-Rage: {}", file, userId, contentRange);
+        log.info("[FileUploadController.upload] uploading file {} for userId {}. Content-Rage: {}",
+                file == null ? "" : file.getOriginalFilename(), userId, contentRange);
 
         Instant now = Instant.now();
 
